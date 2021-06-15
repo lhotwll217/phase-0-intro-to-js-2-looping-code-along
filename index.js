144560fe6ee8716a09e9e3265fa5f705e701a45f
Luke Otwell <lukehenryotwell@gmail.com>
2021-06-15 12:57:39 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Code your solutions in this file
+//const gifts = ["teddy bear", "drone", "doll"];
+
+/*function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+
+    }
+
+    return gifts;
+}
+
+<!-- wrapGifts(gifts);*/
+
+const gifters = ['Lisa', 'Kaitlin', 'Jan'];
+const occasion = 'surprise';
+
+
+function writeCards(gifters, occasion) {
+    let messages = [];
+    for (let i = 0; i < gifters.length; i++) {
+        messages.push(`Thank you, ${gifters[i]}, for the wonderful ${occasion} gift!`);
+    }
+    return messages
+
+
+}
+
+
+function countDown(n) {
+    while (n > -1) {
+        console.log(n);
+        n--;
+
+    }
+
+}
+
